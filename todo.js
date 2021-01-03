2359fe1d495508d5d5e0deaa191700a4a5a78097
Anantha krishnan <ananthankrishnan77@gmail.com>
2021-01-03 11:42:46 +0530

    todo

---
+++ todo.js
@@ -0,0 +1,18 @@
+let addTodo = document.getElementById('addTodo');
+let todoContainer = document.getElementById('todoContainer');
+let inputField = document.getElementById('inputField');
+
+addTodo.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    todoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    });
+    paragraph.addEventListener('dblclick', function(){
+        todoContainer.removeChild(paragraph);
+    });
+    
+});
